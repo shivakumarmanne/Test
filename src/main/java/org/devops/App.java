a4d3c9773c4bac470d39e2cbf0b554b940e083a5
Shiva Kumar Manne <shivakumarmanne@gmail.com>
2020-05-20 11:44:29 +0530

    Update App.java

--- src/main/java/org/devops/App.java
+++ src/main/java/org/devops/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome to Hello World 2.0!" )
+        System.out.println( "Welcome to Hello World 2.0!" );
     }
 }
